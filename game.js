0d8b1c64def3aa73446592a334087b25f56ecfe6
qiufaker-bit <qiufaker@gmail.com>
2026-06-26 19:02:14 +0800

    Add files via upload

---
+++ game.js
@@ -0,0 +1,21 @@
+const { createBalanceDingGame } = require('./src/balance-ding-game.js');
+
+const canvas = wx.createCanvas();
+
+wx.showShareMenu && wx.showShareMenu({ withShareTicket: true });
+
+const game = createBalanceDingGame({
+  canvas,
+  platform: 'wechat',
+  wx,
+  characterSrc: 'assets/rv-rider-character.png'
+});
+
+if (wx.onShareAppMessage) {
+  wx.onShareAppMessage(() => ({
+    title: game.getShareTitle(),
+    imageUrl: ''
+  }));
+}
+
+game.start();
